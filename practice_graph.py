7c22c2eb139dd0c9f303f31e46a2acde46bd2736
linzac03 <linzac03@evergreen.edu>
2013-12-08 23:56:34 -0800

    hw8
    finished prob 2a

--- practice_graph.py
+++ practice_graph.py
@@ -1,14 +1,37 @@
-import networkx
-g = networkx.Graph()
+import networkx as nx
+import matplotlib
 
-g.add_node('a')
-g.add_node('b')
-g.add_node('c')
-g.add_node('d')
-g.add_node('e')
-g.add_node('f')
+rj = nx.Graph()
 
-g.add_edges_from([('a','b'),('a','c')])
-g.add_edges_from([('b','c'),('b','d')])
-g.add_edges_from([('c','d'),('c','f')])
-g.add_edges_from([('d','f'),('d','e')])
+rj.add_edges_from([("Friar Laurence", "Juliet"), ("Friar Laurence", "Romeo")])
+rj.add_edges_from([("Juliet", "Capulet"), ("Juliet", "Romeo"), ("Juliet", "Tybalt"), ("Juliet", "Nurse")])
+rj.add_edges_from([("Romeo", "Juliet"), ("Romeo", "Benvolio"), ("Romeo", "Montague"), ("Romeo", "Mercutio")])
+rj.add_edges_from([("Benvolio", "Montague")])
+rj.add_edges_from([("Capulet", "Tybalt"), ("Capulet", "Escalus"), ("Capulet", "Paris")])
+rj.add_edges_from([("Escalus", "Montague"), ("Escalus", "Mercutio"), ("Escalus", "Paris")])
+rj.add_edges_from([("Paris", "Mercutio"), ("Paris", "Capulet")])
+#networkx.draw(rj)
+#plt.savefig("romeonjuliet2.png")
+def friends(graph, user):
+    """Returns a set of the friends of the given user, in the given graph.
+    The parameter 'user' is the string name of a person in the graph.
+    """
+    return set(graph.neighbors(user))
+    
+def friends_of_friends(graph, user):
+    """Returns a set of friends of friends of the given user, in the given graph.
+    The result does not include the given user nor any of that user's friends.
+    """
+    friends_of = friends(graph, user)
+    f_of_f = []
+    for elem in friends_of:
+        if elem == user:
+            pass
+        else:
+            f_of_f = f_of_f + list(friends(graph, elem))
+    return f_of_f
+
+p = ("yes", "no")
+
+ls = [1,2,3,4,5,6]
+
